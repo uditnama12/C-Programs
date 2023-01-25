aa7e141a96cbc9e2c153ac2a037ad61d80a56614
UDIT KUMAR NAMA <101541118+uditnama12@users.noreply.github.com>
2023-01-25 22:11:21 +0530

    Create AddTwoNo.cpp

---
+++ AddTwoNo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b, sum;
+    cout<<"enter the value of a :";
+    cin>>a;
+    cout<<"Enter the value of b :";
+    cin>>b;
+    sum= a+b;
+    cout<< sum;
+
+return 0;
+}
